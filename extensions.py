9c5042311a25b92a123d367b9627b3db7ee33bbb
TonySavickiy <dgworkmail@yandex.ru>
2023-10-21 19:25:59 +0600

    bot_money

--- extensions.py
+++ extensions.py
@@ -10,28 +10,28 @@ class ConvertExeption(Exception):
 #Описание основной функции конвертации валют.
 class MoneyChange:    
     @staticmethod
-    def get_price(base: str, quote: str, amount: int,float):
+    def get_price(base: str, quote: str, amount):
         try:
             # Проверка на доступность 1-й валюты
             base_key = exchanges[base.lower()]
         except KeyError:
-            raise ConvertExeption(f"Валюта {base} не найдена!")
+            raise ConvertExeption(f"Валюта '{base}' не найдена!")
 
         try:
             # Проверка на доступность 2-й валюты
             quote_key = exchanges[quote.lower()]
         except KeyError:
-            raise ConvertExeption(f"Валюта {quote} не найдена!")
+            raise ConvertExeption(f"Валюта '{quote}' не найдена!")
             
             # Проверка на равенство конвертируемых валют
         if base_key == quote_key:
-            raise ConvertExeption(f'Невозможно перевести одинаковые валюты {base}!')
+            raise ConvertExeption(f'Невозможно перевести одинаковые валюты "{base}" и "{quote}"!')
         
         # Проверка на корректность введенной суммы(должно быть int\float)
         try:
             amount = float(amount)
         except ValueError:
-            raise ConvertExeption(f'Не удалось обработать количество {amount}!')
+            raise ConvertExeption(f'Не удалось обработать количество: "{amount}", кол-во должно быть цифрой!')
 
         # После прохода проверок, генерируем GET запрос к API
         r = requests.get(f'https://openexchangerates.org/api/latest.json?app_id={TOKEN_SEC}')
